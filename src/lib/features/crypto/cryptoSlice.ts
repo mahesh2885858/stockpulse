4c5bc9605464e8b0ceb39f4cab3b4ec518ff77b9
mahesh2885858 <mahesh2885858@gmail.com>
2024-07-14 12:02:00 +0530

    add requried actions

--- src/lib/features/crypto/cryptoSlice.ts
+++ src/lib/features/crypto/cryptoSlice.ts
@@ -1,32 +1,48 @@
+import { TCoin, TCoinFromDb, TCoinNames } from "@/types/cryptoCoin.types";
+import { convertDataToRender } from "@/utils/convertCoinDataToRender";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-export type TCryptoItem = {
-  id: string;
-  symbol: string;
-  name: string;
-  price_usd: string;
-  timeOfData: string;
-  market_cap_usd: string;
-};
+export type TCryptoItem = TCoin & { timeStamp: string };
 
 export type TCryptoState = {
-  value: TCryptoItem[];
+  value: TCoinFromDb;
+  selectedCoinData: TCryptoItem[];
+  selectedCoin: TCoinNames;
+  showCoinChangeModal: boolean;
 };
 
 const initialState: TCryptoState = {
   value: [],
+  selectedCoinData: [],
+  selectedCoin: "Bitcoin",
+  showCoinChangeModal: false,
 };
 
 export const cryptoSlice = createSlice({
   name: "crypto",
   initialState,
   reducers: {
-    initializeWithData: (state, action: PayloadAction<TCryptoItem[]>) => {
+    getNewData: (state, action: PayloadAction<any[]>) => {
       state.value = action.payload;
+    },
+    setShowCoinChangeModal: (state, action: PayloadAction<boolean>) => {
+      state.showCoinChangeModal = action.payload;
+    },
+
+    getDataForSelectedCoin: (state, action: PayloadAction<TCoinNames>) => {
+      const coin = action.payload ? action.payload : state.selectedCoin;
+      const data = convertDataToRender(coin, state.value);
+      state.selectedCoinData = data;
+      state.selectedCoin = coin;
     },
   },
 });
 
-export const { initializeWithData } = cryptoSlice.actions;
+export const {
+  getNewData,
+  setShowCoinChangeModal,
+  // setSelectedCoin,
+  getDataForSelectedCoin,
+} = cryptoSlice.actions;
 
 export default cryptoSlice.reducer;
