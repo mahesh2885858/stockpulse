89a40b2dafd8adfc52550e966ec0d73a35fcb381
mahesh2885858 <mahesh2885858@gmail.com>
2024-07-14 12:07:25 +0530

    wrap children not the body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <StoreProvider>
-        <body className={inter.className}>{children}</body>
-      </StoreProvider>
+      <body className={inter.className}>
+        <StoreProvider>{children}</StoreProvider>
+      </body>
     </html>
   );
 }
