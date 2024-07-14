bcd209bc7858fffac239ef5331fa84190b6efa85
mahesh2885858 <mahesh2885858@gmail.com>
2024-07-15 00:21:33 +0530

    change the api to get the specific coin details

--- src/utils/getTheDataFromApi.ts
+++ src/utils/getTheDataFromApi.ts
@@ -1,16 +1,20 @@
+import { TCoinCodes } from "@/types/cryptoCoin.types";
 import axios from "axios";
 
-const API_URL = "https://api.livecoinwatch.com/coins/list";
+const API_URL = "https://api.livecoinwatch.com/coins/map";
+
+const coinCodes: (keyof TCoinCodes)[] = ["BTC", "BNB", "ETH", "SOL", "USDT"];
 
 export const getTheDataFromApi = async () => {
   try {
     const data = {
+      codes: coinCodes,
       currency: "USD",
       sort: "rank",
       order: "ascending",
       offset: 0,
-      limit: 5,
       meta: true,
+      limit: 0,
     };
 
     const response = await axios.post(API_URL, data, {
