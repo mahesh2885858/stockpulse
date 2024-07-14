c75297ec3a6743db8d70d13238ca57d5eb5efadb
mahesh2885858 <mahesh2885858@gmail.com>
2024-07-15 00:24:14 +0530

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,83 +1,38 @@
 "use client";
+import ChangeButton from "@/components/ChangeButton";
 import CoinChangeModal from "@/components/CoinChangeModal";
+import CryptoTable from "@/components/CryptoTable";
+import Timer from "@/components/Timer";
 import {
+  countTime,
   getDataForSelectedCoin,
   getNewData,
-  setShowCoinChangeModal,
+  resetTimer,
 } from "@/lib/features/crypto/cryptoSlice";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-  const formatDate = (time: string) => {
-    const date = new Intl.DateTimeFormat("en-US", {
-      timeStyle: "short",
-    });
-    return date.format(new Date(time));
-  };
-  const selectedCoinData = useAppSelector((state) => state.selectedCoinData);
   const selectedCoin = useAppSelector((state) => state.selectedCoin);
-  const value = useAppSelector((state) => state.value);
-  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    const getData = async () => {
-      const response = await axios("/api/crypto");
-      const data = response.data;
-      dispatch(getNewData(data.data));
+    const startPolling = async () => {
+      await axios.post("/api/polling");
     };
-    getData();
-
-    const timer = setInterval(() => {
-      getData();
-    }, 1000 * 60);
-
-    return () => {
-      clearInterval(timer);
-    };
+    startPolling();
   }, []);
-
-  useEffect(() => {
-    dispatch(getDataForSelectedCoin(selectedCoin));
-  }, [value]);
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center gap-2  p-12">
+      <main className="flex min-h-screen flex-col items-center gap-2  p-12 bg-[#171b26]">
         <CoinChangeModal />
-        <div className="flex w-3/4 justify-between">
+        <div className="flex w-3/4 justify-between text-white">
           <p>Most recent data on {selectedCoin}</p>
-          <button
-            className="px-2 rounded bg-slate-400 hover:bg-slate-400/50"
-            onClick={() => {
-              console.log("clicked");
-              dispatch(setShowCoinChangeModal(true));
-            }}
-          >
-            Select another coin
-          </button>
+          <Timer />
+          <ChangeButton />
         </div>
-        <table className=" w-3/4">
-          <thead className="">
-            <tr>
-              <th className="text-left pl-4 border">Time</th>
-              <th className="text-left pl-4 border">Price(USD)</th>
-              <th className="text-left pl-4 border">Market cap</th>
-            </tr>
-          </thead>
-          <tbody>
-            {selectedCoinData.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td className="pl-4 border ">{formatDate(item.timeStamp)}</td>
-                  <td className="pl-4 border ">{item.rate}</td>
-                  <td className="pl-4 border ">{item.cap}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <CryptoTable />
       </main>
     </>
   );
