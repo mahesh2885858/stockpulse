ef485981c7bca3c2eeda0d9a5618ac2de6bccb87
mahesh2885858 <mahesh2885858@gmail.com>
2024-07-14 12:09:36 +0530

    clean up and add post method to make it dynamically render

--- src/app/api/crypto/route.ts
+++ src/app/api/crypto/route.ts
@@ -1,26 +1,28 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
 import ConnectToMongo from "@/utils/ConnectToMongo";
 import { getTheDataFromApi } from "@/utils/getTheDataFromApi";
 import mapApiDataToDbData from "@/utils/mapApiDatatoDbData";
 
-let lastUpdatedTime = Date.now();
 let isPolling = false;
 let pollingInterval = 1000 * 60;
 
 export async function GET() {
-  lastUpdatedTime = Date.now();
+  // Start the polling
   if (!isPolling) {
     await pollData();
     await startPolling();
   }
-
   const db = await ConnectToMongo()!;
   if (!db) {
     return Response.json({ error: "Failed to connect to MongoDB" });
   }
   const { client, collection } = db;
   try {
-    const data = await collection.find({}).limit(20).toArray();
+    const data = await collection
+      .find({})
+      .sort({ timeStamp: -1 })
+      .limit(20)
+      .toArray();
+
     return Response.json({ data: data });
   } catch (error) {
     return Response.json({ data: "mahesh-error", error });
@@ -41,17 +43,15 @@ const pollData = async () => {
 
   try {
     const dataFormApi = await getTheDataFromApi();
-
     if (!dataFormApi.success) {
       throw dataFormApi.error;
     }
 
     const dataForDb = mapApiDataToDbData(dataFormApi.data);
-    const data = await collection.insertOne({
+    await collection.insertOne({
       coinData: dataForDb,
       timeStamp: new Date(),
     });
-    console.log("data is being polled");
   } catch (error) {
     console.log({ error });
   } finally {
@@ -62,11 +62,10 @@ const pollData = async () => {
 const startPolling = async () => {
   isPolling = true;
   let timer = setInterval(() => {
-    if (Date.now() - lastUpdatedTime > 10 * 60 * 1000) {
-      clearInterval(timer);
-      isPolling = false;
-      return;
-    }
     pollData();
   }, pollingInterval);
 };
+
+export async function POST() {
+  return Response.json({ test: "working" });
+}
